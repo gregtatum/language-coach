1d064f9b83665d95bb0bf5538c5deded72392ca5
Greg Tatum <tatum.creative@gmail.com>
2023-03-19 09:06:48 -0500

    Adjust the deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,13 @@
-# To set up the deploy to github pages, first run:
-# ----------
-# cd dist
-# git init -b gh-pages
-# git remote add origin git@github.com:gregtatum/REPO.git
 
-cd dist                            \
- && git add .                      \
- && git commit -m "Deploy $(date)" \
- && git push
+# Make the script URL relative.
+#   From: <script defer src="/4c2dfd81560758af273c.bundle.js"></script></head>
+#     To: <script defer src="4c2dfd81560758af273c.bundle.js"></script></head>
+
+sed -i 's|src="/|src="|g' dist/index.html
+
+cd dist                                                                 \
+ && git init -b gh-pages                                                \
+ && git remote add origin git@github.com:gregtatum/translation-asst.git \
+ && git add .                                                           \
+ && git commit -m "Deploy $(date)"                                      \
+ && git push -f
